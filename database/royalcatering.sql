5ebc1e57cdadc31ec69e37bb55da2bf12645d0db
Jignapatel01 <118799751+Jignapatel01@users.noreply.github.com>
2023-09-01 16:38:02 +0530

    Add files via upload

--- database/royalcatering.sql
+++ database/royalcatering.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2023 at 12:57 PM
+-- Generation Time: Aug 10, 2023 at 01:27 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `royalcatering`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_table`
+--
+
+CREATE TABLE `book_table` (
+  `tbl_id` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Date_Time` varchar(255) NOT NULL,
+  `Person` int(11) NOT NULL,
+  `Request` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book_table`
+--
+
+INSERT INTO `book_table` (`tbl_id`, `Name`, `Email`, `Date_Time`, `Person`, `Request`) VALUES
+(1, 'jigna patel', 'jigna@gmail.com', '08/09/2023 12:30 PM', 2, 'Special Event');
 
 -- --------------------------------------------------------
 
@@ -67,8 +89,24 @@ INSERT INTO `tbl_admin` (`admin_id`, `email`, `Password`) VALUES
 --
 
 CREATE TABLE `tbl_cart` (
-  `cart_id` int(11) NOT NULL
+  `cart_id` int(11) NOT NULL,
+  `pro_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `subtotal` int(11) NOT NULL,
+  `added_date` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_cart`
+--
+
+INSERT INTO `tbl_cart` (`cart_id`, `pro_id`, `c_id`, `subtotal`, `added_date`, `status`) VALUES
+(1, 1, 0, 110, '10/08/2023 03:28:48 pm', 'pending'),
+(2, 1, 0, 110, '10/08/2023 03:32:12 pm', 'pending'),
+(3, 5, 0, 50, '10/08/2023 03:32:28 pm', 'pending'),
+(4, 5, 0, 50, '10/08/2023 03:37:15 pm', 'pending'),
+(5, 1, 0, 110, '10/08/2023 04:03:20 pm', 'pending');
 
 -- --------------------------------------------------------
 
@@ -101,6 +139,27 @@ INSERT INTO `tbl_city` (`city_id`, `state_id`, `CityName`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_contact`
+--
+
+CREATE TABLE `tbl_contact` (
+  `contact_id` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Phone` bigint(20) NOT NULL,
+  `Message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_contact`
+--
+
+INSERT INTO `tbl_contact` (`contact_id`, `Name`, `Email`, `Phone`, `Message`) VALUES
+(1, 'jigna', 'jigna@gmail.com', 9856622266, 'thnks for contact with us');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_customer`
 --
 
@@ -112,22 +171,18 @@ CREATE TABLE `tbl_customer` (
   `Password` varchar(255) NOT NULL,
   `Photo` varchar(255) NOT NULL,
   `Gender` varchar(255) NOT NULL,
-  `Phone` bigint(255) NOT NULL,
+  `Phone` bigint(20) NOT NULL,
   `state_id` int(11) NOT NULL,
   `city_id` int(11) NOT NULL,
-  `Registered_date_time` varchar(255) NOT NULL
+  `Registerd_date_time` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_customer`
 --
 
-INSERT INTO `tbl_customer` (`c_id`, `FirstName`, `LastName`, `Email`, `Password`, `Photo`, `Gender`, `Phone`, `state_id`, `city_id`, `Registered_date_time`) VALUES
-(2, 'krupali', 'vaghela', 'krupaliv09@gmail.com', 'a3J1cGExMQ==', 'upload/image/w7.jpg', 'female', 9856235412, 1, 3, '05/06/2023 12:18:36 pm'),
-(3, 'jignasa', 'patel', 'jigna434@gmail.com', 'amlnczEyMw==', 'upload/imageuserprofile.webp', 'female', 7984269823, 1, 1, '18/06/2023 17:54:08 pm'),
-(4, 'abc', 'patel', 'abc12@gmail.com', 'MTIzNDU2', 'upload/image/m6.jpg', 'male', 9865451236, 1, 1, '22/06/2023 07:48:00 am'),
-(5, 'aditya', 'patel', 'aditya@gmail.com', 'MTIzNDU2', 'upload/image/k5.jpg', 'male', 9865451236, 1, 1, '22/06/2023 07:51:19 am'),
-(6, 'bhumit', 'patel', 'bhumit.patel714@gmail.com', 'Ymh1bWl0MTIz', 'upload/image/chef1.webp', 'male', 9875464452, 1, 3, '03/07/2023 18:08:06 pm');
+INSERT INTO `tbl_customer` (`c_id`, `FirstName`, `LastName`, `Email`, `Password`, `Photo`, `Gender`, `Phone`, `state_id`, `city_id`, `Registerd_date_time`) VALUES
+(1, 'jigna', 'patel', 'jigna434@gmail.com', 'amlnbmFwYXRlbA==', 'upload/image/face6.jpg', 'female', 9856321477, 1, 1, '05/08/2023 12:50:47 pm');
 
 -- --------------------------------------------------------
 
@@ -169,8 +224,7 @@ INSERT INTO `tbl_product` (`pro_id`, `subcat_id`, `Product_Name`, `Product_Photo
 (6, 6, 'Cold Coffee', 'upload/product_image/coffee.jpg', 210, '2023-08-04', 'Cold coffee', 1),
 (7, 7, 'Tradistional Dish', 'upload/product_image/traditional dish.jpg', 350, '2023-08-04', 'Best Tradiatioanl dish', 2),
 (8, 10, 'Guj.Dal-rice', 'upload/product_image/dal rice.jpg', 100, '2023-08-04', 'Special Guj Dal-rice', 2),
-(9, 8, 'Veg. Manchurian', 'upload/product_image/veg manchurian.jpg', 180, '2023-08-04', 'Vegitable Manchurian', 3),
-(10, 9, 'Chhole Kulcha', 'upload/product_image/chhole bhature.jpg', 150, '2023-08-04', 'Punjabi special chhoel kulcha', 3);
+(9, 8, 'Veg. Manchurian', 'upload/product_image/veg manchurian.jpg', 180, '2023-08-04', 'Vegitable Manchurian', 3);
 
 -- --------------------------------------------------------
 
@@ -220,11 +274,17 @@ INSERT INTO `tbl_subcategory` (`subcat_id`, `category_id`, `SubCategory_Name`, `
 (7, 2, 'Special Gujarati Dish', '2023-08-04'),
 (8, 3, 'Chinese Food', '2023-08-04'),
 (9, 3, 'Chhole Kulcha', '2023-08-04'),
-(10, 2, 'Dal-rice', '2023-08-04');
+(10, 2, 'Paratha', '2023-08-05');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `book_table`
+--
+ALTER TABLE `book_table`
+  ADD PRIMARY KEY (`tbl_id`);
 
 --
 -- Indexes for table `tbl_addcategory`
@@ -242,7 +302,9 @@ ALTER TABLE `tbl_admin`
 -- Indexes for table `tbl_cart`
 --
 ALTER TABLE `tbl_cart`
-  ADD PRIMARY KEY (`cart_id`);
+  ADD PRIMARY KEY (`cart_id`),
+  ADD KEY `pro_id` (`pro_id`),
+  ADD KEY `c_id` (`c_id`);
 
 --
 -- Indexes for table `tbl_city`
@@ -250,6 +312,12 @@ ALTER TABLE `tbl_cart`
 ALTER TABLE `tbl_city`
   ADD PRIMARY KEY (`city_id`),
   ADD KEY `state_id` (`state_id`);
+
+--
+-- Indexes for table `tbl_contact`
+--
+ALTER TABLE `tbl_contact`
+  ADD PRIMARY KEY (`contact_id`);
 
 --
 -- Indexes for table `tbl_customer`
@@ -291,6 +359,12 @@ ALTER TABLE `tbl_subcategory`
 --
 
 --
+-- AUTO_INCREMENT for table `book_table`
+--
+ALTER TABLE `book_table`
+  MODIFY `tbl_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `tbl_addcategory`
 --
 ALTER TABLE `tbl_addcategory`
@@ -306,7 +380,7 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_cart`
 --
 ALTER TABLE `tbl_cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_city`
@@ -315,10 +389,16 @@ ALTER TABLE `tbl_city`
   MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
+-- AUTO_INCREMENT for table `tbl_contact`
+--
+ALTER TABLE `tbl_contact`
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `tbl_customer`
 --
 ALTER TABLE `tbl_customer`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_order`
@@ -353,6 +433,12 @@ ALTER TABLE `tbl_subcategory`
 --
 ALTER TABLE `tbl_city`
   ADD CONSTRAINT `state_id` FOREIGN KEY (`state_id`) REFERENCES `tbl_state` (`state_id`);
+
+--
+-- Constraints for table `tbl_customer`
+--
+ALTER TABLE `tbl_customer`
+  ADD CONSTRAINT `city_id` FOREIGN KEY (`city_id`) REFERENCES `tbl_city` (`city_id`);
 
 --
 -- Constraints for table `tbl_product`
